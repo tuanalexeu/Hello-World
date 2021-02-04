507b47744dffc552bac938a591f2832f60923f9a
AlexeyTyan <alekseytyan45@gmail.com>
2021-02-04 17:14:35 +0300

    Calculator

--- Hello-World/Program.cs
+++ Hello-World/Program.cs
@@ -3,7 +3,16 @@
 namespace Hello_World {
     internal static class Program {
         private static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Calc calc = new Calc();
+            int ans = calc.Add(10, 84);
+            Console.WriteLine("10 + 84 is {0}", ans);
+            Console.ReadLine();
+        }
+    }
+
+    class Calc {
+        public int Add(int x, int y) {
+            return x + y;
         }
     }
 }
